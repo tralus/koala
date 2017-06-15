792f2785d9f1e2b52313937578ef17325e4386a7
phenrigomes <phenrigomes@gmail.com>
2017-06-15 15:03:33 -0300

    Change the config to use the package viper.

--- config/config.go
+++ config/config.go
@@ -1,13 +1,10 @@
 package config
 
 import (
-	"bytes"
-	"io/ioutil"
+	"fmt"
 	"os"
-	"path"
-	"text/template"
 
-	"gopkg.in/yaml.v2"
+	"github.com/spf13/viper"
 )
 
 // ConfigFilename settings filename
@@ -18,7 +15,19 @@ func init() {
 	ConfigFilename = os.Getenv("CONFIG_FILENAME")
 
 	if len(ConfigFilename) == 0 {
-		ConfigFilename = "app.yml"
+		ConfigFilename = "app"
+	}
+
+	// Name of config file (without extension)
+	viper.SetConfigName(ConfigFilename)
+	viper.AddConfigPath("./config")
+	viper.AutomaticEnv()
+
+	// Find and read the config file
+	err := viper.ReadInConfig()
+
+	if err != nil { // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
 	}
 }
 
@@ -41,70 +50,18 @@ type Config struct {
 	}
 }
 
-var tplFuncMap = template.FuncMap{
-	"env": getEnvVar,
+// Viper returns the viper instance
+func (c Config) Viper() *viper.Viper {
+	return viper.GetViper()
 }
 
-// getEnvVar gets an environment variable or the default value
-func getEnvVar(v string, d string) string {
-	r := os.Getenv(v)
-
-	if r != "" {
-		return r
-	}
-
-	return d
-}
-
-// ParseSettingsFile parse the settings file
-// The parse can override some values as environment variables
-func ParseSettingsFile(text string) ([]byte, error) {
-	template := template.New("koalasettingstpl")
-
-	template, err := template.Funcs(tplFuncMap).Parse(text)
-
-	if err != nil {
-		return nil, err
-	}
-
-	var buffer bytes.Buffer
-
-	template.Execute(&buffer, nil)
-
-	return buffer.Bytes(), nil
-}
-
-// LoadConfigFromBytes loads the application settings from bytes
-// It allows that binary data can be used
-func LoadConfigFromBytes(b []byte) (Config, error) {
+// LoadConfig loads the application settings
+func LoadConfig() (Config, error) {
 	var c Config
 
-	if err := yaml.Unmarshal(b, &c); err != nil {
+	if err := viper.Unmarshal(&c); err != nil {
 		return c, err
 	}
 
 	return c, nil
-}
-
-// LoadConfig loads the application settings
-func LoadConfig() (c Config, err error) {
-	bytes, err := ReadConfigFile()
-
-	if err != nil {
-		return c, err
-	}
-
-	bytes, err = ParseSettingsFile(string(bytes))
-
-	if err != nil {
-		return c, err
-	}
-
-	return LoadConfigFromBytes(bytes)
-}
-
-// ReadConfigFile reads the settings file per convention
-// The file should be stored inside config folder in the project root
-func ReadConfigFile() ([]byte, error) {
-	return ioutil.ReadFile(path.Join("config", ConfigFilename))
 }
